b41dfc80bb34ab984d74ebbfae050f0ea817c26d
Nathan Pearson <nate.m.pearson@gmail.com>
2018-08-30 08:33:54 -0400

    Initial commit

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        //from 1 to 100 
+        //print fizz for any multiple of 3
+        //print buzz for and multiple of 5
+        //and fizzbuzz for any multiple of 3 & 5
+        //perhaps later return to make it multiples of A & B
+
+        static void Main(string[] args)
+        {
+            string f = "fizz";
+            string b = "buzz";
+
+            for (int i = 1; i <= 100; i++)
+            {
+                string res = "";
+                if (i % 3 == 0) { res += f; }
+                if (i % 5 == 0) { res += b; }
+                Console.WriteLine(i + " " + res);
+            }
+        }
+    }
+}
